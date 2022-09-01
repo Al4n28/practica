ce4677e844fd7f80c326d33a79c389e9200b80b6
Pablo Schwarzenberg <pabloschwarzenberg@gmail.com>
2022-09-01 12:38:06 -0400

    ejemplo makefile

--- guia_01/ejer01_guia01.c
+++ guia_01/ejer01_guia01.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 
-float Calcular_Precio_Descuento( float Precio, float Descuento){
+float Calcular_Precio_Descuento( float Precio, float Descuento);
 
-    /* Forma Correcta de hacerlo
-    float Descuento_Porcentual = Descuento/100;
-    float Descuento_A_Descontar = Precio*Descuento_Porcentual;
-    float Total = Precio-Descuento_A_Descontar;
-    */
-   
-    //Como yo lo haría:
-    return Precio*(1-(Descuento/100));
-  
-}
+#define BOOST_TEST_MODULE example
+#include </Users/psr/Desktop/paper7-GNN/practica//boost/test/included/unit_test.hpp>
 
-int main() {
+BOOST_AUTO_TEST_SUITE( test_suite1 )
+
+BOOST_AUTO_TEST_CASE( test_case1 )
+{
   float r =Calcular_Precio_Descuento(1000,10);
-  printf("%.2f",r);
-  return 0;
-
+  BOOST_TEST(r == 900);
 }
+
+BOOST_AUTO_TEST_CASE( test_case2 )
+{
+  float r =Calcular_Precio_Descuento(1000,100);
+  BOOST_TEST(r == 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE( test_suite2 )
+
+BOOST_AUTO_TEST_CASE( test_case3 )
+{
+  float r =Calcular_Precio_Descuento(1000,0);
+  BOOST_TEST(r == 1000);
+}
+
+BOOST_AUTO_TEST_CASE( test_case4 )
+{
+  float r =Calcular_Precio_Descuento(1000,200);
+  BOOST_TEST(r == -1000);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
